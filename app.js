3514befeb31087c74ae571814c69e23f59e383ab
dabc <esven96@yahoo.com>
2015-05-17 01:59:58 -0400

    added cors support

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
+var cors = require('cors');
 
 var routes = require('./routes/index');
 
 var api = require('./routes/api');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
